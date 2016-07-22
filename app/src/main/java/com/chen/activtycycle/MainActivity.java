77ae32bf0ae05403e7b583c2249c4e1ac09ad8d8
ckd <ckd@163.com>
2016-07-22 16:25:26 +0800

    显示在屏幕低端

--- app/src/main/java/com/chen/activtycycle/MainActivity.java
+++ app/src/main/java/com/chen/activtycycle/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
-                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
             }
         });
     }
