c3b605e961335eb3c25de31acace80ba8ec5e52d
ckd <ckd@163.com>
2016-07-22 10:54:45 +0800

    活动生命

--- app/src/main/java/com/chen/activtycycle/MainActivity.java
+++ app/src/main/java/com/chen/activtycycle/MainActivity.java
@@ -1,8 +1,11 @@
 package com.chen.activtycycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -11,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity","on create执行了");
+        Button button = (Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -25,5 +36,22 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onResume:执行了 ");
     }
 
-    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: 执行了");
+        
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: 执行了");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: 执行了");
+    }
 }
