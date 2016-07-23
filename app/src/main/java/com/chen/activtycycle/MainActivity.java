d025ad1c598dab0f86caa0ee84ecf833f128cbeb
ckd <ckd@163.com>
2016-07-23 10:27:21 +0800

    显示TextView

--- app/src/main/java/com/chen/activtycycle/MainActivity.java
+++ app/src/main/java/com/chen/activtycycle/MainActivity.java
@@ -6,21 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public static final String BOOK_NAME_CATEGORY_PHISICAL = "BookOfCategoryPhisical";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity","on create执行了");
         Button button = (Button) findViewById(R.id.activity_main_button);
+        final EditText editText = (EditText)findViewById(R.id.editText_1) ;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick:");
                 Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
-                intent.putExtra("name", "xxxxxxxx");
+                intent.putExtra(BOOK_NAME_CATEGORY_PHISICAL, editText.getText().toString());
                 startActivity(intent);
             }
         });
